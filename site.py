4f04c3ef753e05be2e476623cb5c6cc7b21d2350
Roman <frideriko26_93@mail.ru>
2025-03-01 14:07:18 +0300

    my site

--- site.py
+++ site.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 
 @app.route('/')
 def index():
@@ -8,7 +8,7 @@ def index():
 
 @app.route('/certificate/python')
 def python_certificate():
-    return render_template('certificate_python.html')
+    return render_template('templates/certificate_python.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
